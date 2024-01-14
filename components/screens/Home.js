d34902223bd90bdfc69371320bb41ecd04aab438
Usama Khan <usamaoff796@gmail.com>
2024-01-15 00:15:32 +0500

    Learn how to pass data through screens also learn how to destructure objects and arrays.

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,11 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
+
+import styles from "../../styles/styles";
+
 /* eslint-disable prettier/prettier */
 const { View, Text, StyleSheet } = require("react-native");
 
-const Home = () => {
+const Home = (props) => {
+
+    const { name, age, martialStatus } = props.route.params;
     return (
         <View style={homeStyle.main}>
             <Text>Home Screen</Text>
+            <Text style={homeStyle.text}>Name: {name}</Text>
+            <Text style={homeStyle.text}>Age: {age}</Text>
+            <Text style={homeStyle.text}>Martial Status: {martialStatus}</Text>
         </View>
     );
 }
@@ -15,6 +23,10 @@ const homeStyle = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1
+    },
+    text: {
+        color: '#000',
+        fontSize: 22
     }
 });
 
--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,12 +1,30 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable prettier/prettier */
+import { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 const Login = (props) => {
+
+    const name = "Usama";
+    const age = 24;
+    const [martialStatus, setMartialStatus] = useState("Single");
+
+    useEffect(() => {
+        if (martialStatus === "Mingle") {
+            props.navigation.navigate('Home', { name, age, martialStatus });
+        }
+
+        return () => {
+            setMartialStatus("Single")
+        }
+    }, [martialStatus]);
+
+
     return (
         <View style={loginStyle.main}>
             <Text>Login Screen</Text>
-            <Button title="Goto Home Screen" onPress={() => props.navigation.navigate('Home')} />
+            <Text>{martialStatus}</Text>
+            <Button title="Goto Home Screen" onPress={() => setMartialStatus("Mingle")} />
         </View>
     );
 }
