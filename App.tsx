b1356dc9ae6d416d2291cce2caa5a41ea66702da
Usama Khan <usamaoff796@gmail.com>
2024-01-14 14:23:25 +0500

    Learn how to change statusbar color and it's content color also learn Platform conditions handling Like if IOS then color should be red else android color should be blue.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react-native/no-inline-styles */
-import {View} from "react-native";
+import {Platform, StatusBar, View} from "react-native";
 import Pressablecomp from "./components/Pressablecomp";
 
 function App(): JSX.Element {
   return (
     <View style={{flex: 1}}>
+      <StatusBar
+        backgroundColor={Platform.OS === "android" ? "blue" : "yellow"}
+        barStyle={"light-content"}
+        hidden={false}
+      />
       <Pressablecomp />
     </View>
   );
