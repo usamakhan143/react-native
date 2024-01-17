ebfd5bec4940a6de88003371109bc54cb8fce291
Usama Khan <usamaoff796@gmail.com>
2024-01-18 01:43:21 +0500

    Learn TopTab Navigation, BottomTab Navigation and Stack Navigation.

--- App.tsx
+++ App.tsx
@@ -7,9 +7,16 @@ import {NavigationContainer} from "@react-navigation/native";
 import Home from "./components/screens/Home";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Login from "./components/screens/Login";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
+import Grid from "./components/Grid";
+import User from "./components/User";
+import Togglecom from "./components/Togglecom";
 
 function App(): JSX.Element {
   const Stack = createNativeStackNavigator();
+  const bottomTab = createBottomTabNavigator();
+  const TopTab = createMaterialTopTabNavigator();
 
   return (
     <>
@@ -18,7 +25,9 @@ function App(): JSX.Element {
         barStyle={"light-content"}
         hidden={false}
       />
-      <NavigationContainer>
+
+      {/* Stack Navigation */}
+      {/* <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
             headerStyle: {
@@ -47,6 +56,24 @@ function App(): JSX.Element {
           />
           <Stack.Screen name={"Home"} component={Home} />
         </Stack.Navigator>
+      </NavigationContainer> */}
+
+      {/* Top Tab Navigation */}
+      {/* <NavigationContainer>
+        <TopTab.Navigator>
+          <TopTab.Screen name="Login" component={Grid} />
+          <TopTab.Screen name="Home" component={User} />
+          <TopTab.Screen name="Status" component={Togglecom} />
+        </TopTab.Navigator>
+      </NavigationContainer> */}
+
+      {/* Bottom Tab Navigation */}
+      <NavigationContainer>
+        <bottomTab.Navigator>
+          <bottomTab.Screen name="Login" component={Grid} />
+          <bottomTab.Screen name="Home" component={User} />
+          <bottomTab.Screen name="Status" component={Togglecom} />
+        </bottomTab.Navigator>
       </NavigationContainer>
     </>
   );
